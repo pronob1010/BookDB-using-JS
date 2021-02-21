fcda46d5d28cfeb1f87e3e0e7478e6ba35fbc579
Pronob <pronobmozumder.info@gmail.com>
2021-02-22 01:20:26 +0600

    alert problem solved

--- js/script.js
+++ js/script.js
@@ -47,52 +47,55 @@ class UI {
             document.querySelector('.alert').remove();
         }, 3000);
     }
-    deleteFromBook(target){
-        if (target.hasAttribute('href')){
+    deleteFromBook(target) {
+        if (target.hasAttribute('href')) {
+
+            let ui = new UI();
+            ui.showAlert('Book Removed!', 'success');
+
             target.parentElement.parentElement.remove();
         }
         // console.log(target);
     }
 
-        clearfields() {
-            let title = document.querySelector("#title").value = '';
-            let author = document.querySelector("#author").value = '';
-            let isbn = document.querySelector("#isbn").value = '';
-        }
+    clearfields() {
+        let title = document.querySelector("#title").value = '';
+        let author = document.querySelector("#author").value = '';
+        let isbn = document.querySelector("#isbn").value = '';
     }
+}
 
-    form.addEventListener('submit', newBook);
-    bookList.addEventListener('click', removeBook);
+form.addEventListener('submit', newBook);
+bookList.addEventListener('click', removeBook);
 
-    function newBook(e) {
-        // console.log("Hello");
-        let title = document.querySelector("#title").value;
-        let author = document.querySelector("#author").value;
-        let isbn = document.querySelector("#isbn").value;
+function newBook(e) {
+    // console.log("Hello");
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let isbn = document.querySelector("#isbn").value;
 
-        let ui = new UI();
+    let ui = new UI();
 
-        if (title === '' || author === "" || isbn === "") {
-            // alert("dfdf");
-            ui.showAlert("Something is wrong !!!", "error");
-        } else {
+    if (title === '' || author === "" || isbn === "") {
+        // alert("dfdf");
+        ui.showAlert("Something is wrong !!!", "error");
+    } else {
 
-            let book = new Book(title, author, isbn);
-            // console.log(book);
+        let book = new Book(title, author, isbn);
+        // console.log(book);
 
-            ui.addBookList(book);
-            ui.clearfields();
-            ui.showAlert("Successfully added!!!", "success");
+        ui.addBookList(book);
+        ui.clearfields();
+        ui.showAlert("Successfully added!!!", "success");
 
-        }
-        e.preventDefault();
     }
+    e.preventDefault();
+}
 
-    function removeBook(e){
-        
-        let ui = new UI();
+function removeBook(e) {
 
-        ui.deleteFromBook(e.target);
-        ui.showAlert('Book Removed!', 'success');
-        e.preventDefault();
-    }
+    let ui = new UI();
+
+    ui.deleteFromBook(e.target);
+    e.preventDefault();
+}
